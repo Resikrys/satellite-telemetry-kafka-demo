c75f06cc38fbe410d3fdbc9b16f57d476c166318
resikrys <resicris@gmail.com>
2026-05-04 10:45:39 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# SATELLITE TELEMETRY KAFKA DEMO
+
+This project simulates a ground segment system using event-driven 
+microservices to process satellite telemetry in a scalable and 
+resilient way.
---
+++ src/test/java/com/satellite_kafka_demo/SatelliteKafkaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.satellite_kafka_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SatelliteKafkaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
