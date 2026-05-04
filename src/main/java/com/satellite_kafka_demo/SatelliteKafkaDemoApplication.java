58483e67e50acb03a9bc9d541a3e4e9080b07e56
resikrys <resicris@gmail.com>
2026-05-04 11:43:12 +0200

    add enabledScheduling annotation

--- src/main/java/com/satellite_kafka_demo/SatelliteKafkaDemoApplication.java
+++ src/main/java/com/satellite_kafka_demo/SatelliteKafkaDemoApplication.java
@@ -2,7 +2,9 @@ package com.satellite_kafka_demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class SatelliteKafkaDemoApplication {
 
